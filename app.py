5cb2f39a8cede62f5f8c0f23fa7464aad5731896
Vero <veronicamairenaoliva@fp.iesromerovargas.com>
2022-03-27 14:44:37 +0200

    Primer commit. Añadir archivos

---
+++ app.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, render_template
+from datetime import datetime
+import locale
+
+app = Flask(__name__)
+locale.setlocale(locale.LC_TIME, '')
+
+
+@app.route("/")
+def index():
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # Increment the count
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+
+  
+
+    the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
+
+    # Render HTML with variable
+    return render_template("index.html", the_time=the_time, tema="dogs", count=count)
+
+   
+if __name__ == "__main__":
+    app.run()
