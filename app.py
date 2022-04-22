de6c99ee47a17dc0a0af7357eebe8d9dc166e4ed
Vero <veronicamairenaoliva@fp.iesromerovargas.com>
2022-04-22 12:11:05 +0200

    Corregido app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def index():
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
     # Render HTML with variable
     return render_template("index.html", the_time=the_time, tema="dogs", count=count)
-    
+
 
 if __name__ == "__main__":
     app.run()
